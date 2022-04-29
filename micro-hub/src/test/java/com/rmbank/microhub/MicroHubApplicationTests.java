58e060de980f3b0f4c3979ef1ddead536b9353c2
renanmuniz <renan.merlo@paygo.com.br>
2022-04-29 00:38:30 -0300

    Criacao do projeto

---
+++ micro-hub/src/test/java/com/rmbank/microhub/MicroHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rmbank.microhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
