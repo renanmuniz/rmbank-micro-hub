4763cdae086301381da21041af487a04d9f6cd3a
renanmuniz <renan.merlo@paygo.com.br>
2022-05-06 00:33:25 -0300

    [fix] : removido swagger

--- micro-hub/src/main/java/com/rmbank/microhub/MicroHubApplication.java
+++ micro-hub/src/main/java/com/rmbank/microhub/MicroHubApplication.java
@@ -3,15 +3,13 @@ package com.rmbank.microhub;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSpringDataWebSupport
-// @EnableSwagger2 FIXME Swagger nao esta deixando a aplicacao subir!
 public class MicroHubApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MicroHubApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MicroHubApplication.class, args);
+    }
 
 }
